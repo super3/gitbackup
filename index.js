c1c473e766c396829856a280f00fb63581473bb7
Shawn Wilkinson <me@super3.org>
2019-07-27 18:26:01 -0400

    Working with new lib

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const fs = require("fs");
-var ndjson = require('ndjson');
+const redis = require('./redis.js');
+const ndjson = require('iterable-ndjson')
+const fs = require('fs')
+const source = fs.createReadStream('./users.json')
 
-fs.createReadStream('./users.json')
-  .pipe(ndjson.parse())
-  .on('data', function(obj) {
+async function import_users() {
+  let userIndex = 0;
+
+  for await (const obj of ndjson.parse(source)) {
     let user = obj.actor_login;
-    console.log("user: " + obj.actor_login);
+    console.log(`user(${userIndex}): ${user}`);
+    await redis.set(`users:${userIndex}`, user);
+    userIndex++;
+  }
 
-    substr = "";
-    for (var i = 0; i < user.length; i++) {
-        substr += user.charAt(i);
-        console.log(substr);
-    }
-  })
+  process.exit(0);
+};
+
+import_users();
+
+// substr = "";
+// for (var i = 0; i < user.length; i++) {
+//     substr += user.charAt(i);
+//     //console.log(substr);
+// }
