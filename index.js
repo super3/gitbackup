393b730d09ecb0f168c5e2d37b059e851ca6fe24
Shawn Wilkinson <me@super3.org>
2019-07-27 17:12:51 -0400

    Cleanup

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ var ndjson = require('ndjson');
 fs.createReadStream('./users.json')
   .pipe(ndjson.parse())
   .on('data', function(obj) {
-    // obj is a javascript object
+    let user = obj.actor_login;
     console.log("user: " + obj.actor_login);
+
     substr = "";
-    for (var i = 0; i < obj.actor_login.length; i++) {
-        substr += obj.actor_login.charAt(i);
+    for (var i = 0; i < user.length; i++) {
+        substr += user.charAt(i);
         console.log(substr);
     }
   })
