28a0c904837806872ec1bf0b47dcc10bdec1102d
Shawn Wilkinson <me@super3.org>
2019-08-02 13:17:33 -0400

    XO Fixes

--- index.js
+++ index.js
@@ -1,69 +1,76 @@
-const fs = require('fs')
+const fs = require('fs');
 const axios = require('axios');
-const redis = require('./redis.js');
-var dateFormat = require('dateformat');
+const dateFormat = require('dateformat');
 const ndjson = require('iterable-ndjson');
+const redis = require('./redis.js');
 
-async function indexSubStrs (user) {
-  let substr = "";
+async function indexSubStrs(user) {
+	let substr = '';
 
-  // create substrings for given username (not including the username itself)
-  for (var i = 0; i < user.length - 1; i++) {
-      // convert index to lowercase, but keep results in original case
-      substr += user.toLowerCase().charAt(i);
+	// Create substrings for given username (not including the username itself)
+	for (let i = 0; i < user.length - 1; i++) {
+		// Convert index to lowercase, but keep results in original case
+		substr += user.toLowerCase().charAt(i);
 
-      // count the number of items in the index list
-      let numRes = await redis.scard(`index:${substr}:users`);
-      // if there are 5 or less items then add substring to index list
-      if (numRes <= 5) await redis.sadd(`index:${substr}:users`, user);
-  }
+		// Count the number of items in the index list
+		const numRes = await redis.scard(`index:${substr}:users`);
+		// If there are 5 or less items then add substring to index list
+		if (numRes <= 5) {
+			await redis.sadd(`index:${substr}:users`, user);
+		}
+	}
 
-  // always add the full username to the index, and since its a
-  // set there should not be any duplicates :)
-  await redis.sadd(`index:${user.toLowerCase()}:users`, user);
+	// Always add the full username to the index, and since its a
+	// set there should not be any duplicates :)
+	await redis.sadd(`index:${user.toLowerCase()}:users`, user);
 }
 
 async function importUsers(usersFile) {
-  let userIndex = 0;
-  const source = fs.createReadStream(usersFile);
+	let userIndex = 0;
+	const source = fs.createReadStream(usersFile);
 
-  // parse usernames from file, and index them in Redis
-  for await (const obj of ndjson.parse(source)) {
-    let user = obj.actor_login;
-    if ((userIndex % 100) == 0) console.log(`user(${userIndex}): ${user}`);
-    await indexSubStrs(user);
-    userIndex++;
-  }
+	// Parse usernames from file, and index them in Redis
+	for await (const obj of ndjson.parse(source)) {
+		const user = obj.actor_login;
+		if ((userIndex % 100) == 0) {
+			console.log(`user(${userIndex}): ${user}`);
+		}
 
-  process.exit(0);
-};
+		await indexSubStrs(user);
+		userIndex++;
+	}
 
-//importUsers('./user_dumps/sample_users.json');
+	process.exit(0);
+}
 
-//importUsers('./user_dumps/github_users_2015.json'); // 1/1/2015
-//importUsers('./user_dumps/github_users_2016.json');
-//importUsers('./user_dumps/github_users_2017.json');
-//importUsers('./user_dumps/github_users_2018.json');
-//importUsers('./user_dumps/github_users_2019.json'); // 7/25/2019
+// ImportUsers('./user_dumps/sample_users.json');
+
+// importUsers('./user_dumps/github_users_2015.json'); // 1/1/2015
+// importUsers('./user_dumps/github_users_2016.json');
+// importUsers('./user_dumps/github_users_2017.json');
+// importUsers('./user_dumps/github_users_2018.json');
+// importUsers('./user_dumps/github_users_2019.json'); // 7/25/2019
 
 // TODO: Download, process, and index new usernames from gharchive.org
 
-const startDate = new Date("2019-07-25");
+const startDate = new Date('2019-07-25');
 async function syncUsers(startDate) {
-  const endDate = new Date(); // today
+	const endDate = new Date(); // Today
 
-  var loop = startDate;
-  while(loop <= endDate){
-    let target = dateFormat(loop, "yyyy-mm-dd");
-    // loop through 24 hours
-    for (let i = 0; i <= 24; i++) {
-      //await axios.get(`http://data.gharchive.org/${target}-${i}.json.gz`);
-      console.log(`http://data.gharchive.org/${target}-${i}.json.gz`);
-    }
-    var newDate = loop.setDate(loop.getDate() + 1);
-    loop = new Date(newDate);
-  }
+	let loop = startDate;
+	while (loop <= endDate) {
+		const target = dateFormat(loop, 'yyyy-mm-dd');
+		// Loop through 24 hours
+		for (let i = 0; i <= 24; i++) {
+			// Await axios.get(`http://data.gharchive.org/${target}-${i}.json.gz`);
+			console.log(`http://data.gharchive.org/${target}-${i}.json.gz`);
+		}
 
-  redis.quit();
+		const newDate = loop.setDate(loop.getDate() + 1);
+		loop = new Date(newDate);
+	}
+
+	redis.quit();
 }
+
 syncUsers(startDate);
