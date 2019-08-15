2979e1e003b46014094ce8f00563891dc0b2b481
Shawn Wilkinson <me@super3.org>
2019-08-15 10:54:55 -0400

    Add IsValidUser Endpoint

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const axios = require('axios');
 const redis = require('./redis.js');
 
 const app = new Koa();
@@ -10,17 +11,23 @@ async function partialUserSearch(input) {
 	return redis.smembers(`index:${input}:users`);
 }
 
-router.get('/', ctx => {
-	ctx.body = 'Hello World!';
-});
-
 router.get('/users/:partialUser', async ctx => {
 	ctx.body = await partialUserSearch(ctx.params.partialUser);
 });
 
+router.get('/isvaliduser/:partialUser', async ctx => {
+	try {
+		await axios.get(`https://github.com/${ctx.params.partialUser}`);
+		ctx.body = 'true';
+	} catch(error) {
+		ctx.body = 'false';
+	}
+});
+
 app
 	.use(router.routes())
-	.use(router.allowedMethods());
+	.use(router.allowedMethods())
+	.use(require('koa-static')(`./`));
 
 app.listen(PORT, () => {
 	console.log('Server running on port ' + PORT + '...');
