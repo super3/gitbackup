25fc1efdfe774aaa6e967b73d1f16a5931f0541f
Shawn Wilkinson <me@super3.org>
2019-08-16 15:08:27 -0400

    Add User Works

--- server.js
+++ server.js
@@ -11,17 +11,36 @@ async function partialUserSearch(input) {
 	return redis.smembers(`index:${input}:users`);
 }
 
+async function githubUserExists(partialUser) {
+	try {
+		await axios.get(`https://github.com/${partialUser}`);
+		return true;
+	} catch(error) {
+		return false;
+	}
+}
+
 router.get('/users/:partialUser', async ctx => {
 	ctx.body = await partialUserSearch(ctx.params.partialUser);
 });
 
 router.get('/isvaliduser/:partialUser', async ctx => {
-	try {
-		await axios.get(`https://github.com/${ctx.params.partialUser}`);
-		ctx.body = 'true';
-	} catch(error) {
-		ctx.body = 'false';
+	ctx.body = await githubUserExists(ctx.params.partialUser);
+});
+
+router.get('/adduser/:user', async ctx => {
+	const userExists = await githubUserExists(ctx.params.user);
+
+	if (userExists) {
+		 const added = await redis.sadd('tracked', ctx.params.user) === 1;
+		 ctx.body = added ? 'added' : 'exists';
+	} else {
+		ctx.throw(500, "User/organization doesn't exist!");
 	}
+});
+
+router.get('/userlist/:page', async ctx => {
+	ctx.body = await redis.smembers('tracked');
 });
 
 app
