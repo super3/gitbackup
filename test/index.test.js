89c039ff72b62c1725a4fee617059e5a656de248
Monty Anderson <work@montyanderson.net>
2019-08-24 13:48:18 +0100

    test for indexUser

--- test/index.test.js
+++ test/index.test.js
@@ -7,4 +7,25 @@ test('toSubstrings', () => {
 
 test('indexUser', async () => {
 	await scraper.indexUser('testusername');
+
+	expect(await redis.sismember('index:t:users', 'testusername')).toBe(1);
+	expect(await redis.sismember('index:te:users', 'testusername')).toBe(1);
+	expect(await redis.sismember('index:tes:users', 'testusername')).toBe(1);
+	expect(await redis.sismember('index:test:users', 'testusername')).toBe(1);
+	expect(await redis.sismember('index:testu:users', 'testusername')).toBe(1);
+	expect(await redis.sismember('index:testus:users', 'testusername')).toBe(1);
+	expect(await redis.sismember('index:testuse:users', 'testusername')).toBe(1);
+	expect(await redis.sismember('index:testuser:users', 'testusername')).toBe(1);
+	expect(await redis.sismember('index:testusern:users', 'testusername')).toBe(1);
+	expect(await redis.sismember('index:testuserna:users', 'testusername')).toBe(1);
+	expect(await redis.sismember('index:testusernam:users', 'testusername')).toBe(1);
+	expect(await redis.sismember('index:testusername:users', 'testusername')).toBe(1);
+
+	await scraper.indexUser('testusername-1');
+	await scraper.indexUser('testusername-2');
+	await scraper.indexUser('testusername-3');
+	await scraper.indexUser('testusername-4');
+	await scraper.indexUser('testusername-5');
+
+	expect(await redis.sismember('index:testusername-5:users', 'testusername')).toBe(0);
 });
