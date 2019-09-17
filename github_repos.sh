ddd7f369adbc90681736b3bfa20299168d332d98
Monty Anderson <work@montyanderson.net>
2019-09-17 14:39:29 +0100

    du ./

--- github_repos.sh
+++ github_repos.sh
@@ -24,7 +24,7 @@ function users_from_file() {
         get_user_repos $user;
         download_user_repos $user;
 
-		storage=$(du -s repos -B1 | cut -f1);
+		storage=$(du -s ./ -B1 | cut -f1);
 		redis-cli set "stats:storage" "$storage";
 
 		files=$(find "." "!" -name '.*' -type f | wc -l);
