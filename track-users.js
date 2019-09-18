ced2329af39cb18cf8d6d725a4343f6484dde33b
Monty Anderson <work@montyanderson.net>
2019-09-18 14:45:21 +0100

    track users

---
+++ track-users.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const ndjson = require('iterable-ndjson');
+const redis = require('./redis');
+
+const file = process.argv[2];
+const limit = Number(process.argv[3]) || Infinity;
+
+(async () => {
+	let i = 0;
+
+	const source = fs.createReadStream(file);
+
+	for await (const obj of ndjson.parse(source)) {
+		if(i++ > limit) {
+			break;
+		}
+
+		await redis.sadd('tracked', obj.actor_login);
+	}
+
+	process.exit(0);
+})();
