a67318e244f4dc0f431e6c15df4eabc10008dfe7
Shawn Wilkinson <me@super3.org>
2019-09-26 15:32:34 -0400

    Remove Comment

--- github_repos.sh
+++ github_repos.sh
@@ -23,16 +23,6 @@ function users_from_file() {
         echo "Backing up user: ${user}";
         get_user_repos $user;
         download_user_repos $user;
-
-		# storage=$(du -s ./ -B1 | cut -f1);
-		# files=$(find "." "!" -name '.*' -type f | wc -l);
-		# repos=$(find . -mindepth 2 -maxdepth 2 -type d | wc -l);
-		# users=$(find . -mindepth 1 -maxdepth 1 -type d | wc -l);
-		#
-		# redis-cli set "stats:storage" "$storage";
-		# redis-cli set "stats:files" "$files";
-		# redis-cli set "stats:repos" "$repos"
-		# redis-cli set "stats:users" "$users"
     done
 }
 
