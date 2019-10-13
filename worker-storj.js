9fc569d5f1b52d21a0a323d5f8465a8c82dd505b
Monty Anderson <work@montyanderson.net>
2019-10-13 17:39:17 +0100

    Storj support

---
+++ worker-storj.js
@@ -0,0 +1,139 @@
+const fs = require('fs').promises;
+const axios = require('axios');
+const git = require('nodegit');
+const execa = require('execa');
+
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+
+if(typeof client_id !== 'string' || typeof client_secret !== 'string') {
+	throw new Error('No API keys set!');
+}
+
+async function cloneUser(username) {
+	const repos = [];
+
+	// get all repositories
+
+	for(let i = 1; ; i++) {
+		const {data} = await axios.get(`https://api.github.com/users/${username}/repos`, {
+			params: {
+				page: i,
+				per_page: 100,
+				client_id,
+				client_secret
+			}
+		});
+
+		if(data.length === 0) {
+			break;
+		}
+
+		repos.push(...data);
+	}
+
+	console.log(username, 'has', repos.length, 'repositories');
+
+	for(const repo of repos) {
+		const repoPath = `${__dirname}/repos/${repo.full_name}`;
+
+		let exists = true;
+
+		try {
+			await fs.stat(repoPath);
+		} catch(err) {
+			exists = false;
+		}
+
+		if(exists === true) {
+			// already exists
+			console.log(repo.full_name, 'already exists, fetching');
+
+			const gitRepo = await git.Repository.open(`${repoPath}/.git`);
+
+			try {
+				await gitRepo.fetchAll();
+			} catch(err) {
+				console.log('fetch failed');
+			}
+		} else {
+			// clone from fresh
+			console.log(repo.full_name, 'cloning from fresh');
+
+			try {
+				await git.Clone(repo.git_url, repoPath);
+			} catch(err) {
+				console.log('clone failed');
+			}
+		}
+
+		const repoZip = `${repoPath}.zip`;
+
+		try {
+			await fs.stat(repoZip);
+			await fs.unlink(repoZip)
+		} catch(err) {
+
+		}
+
+		// only create zip if repo cloned (not empty)
+		let cloned = false;
+
+		try {
+			await fs.stat(repoPath);
+			cloned = true;
+		} catch(err) {
+
+		}
+
+		if(cloned === true) {
+			await execa('zip', [ '-r', repoZip, './' ], {
+				cwd: repoPath
+			});
+		}
+
+		await execa(`${__dirname}/uplink_linux_amd64`, [ 'cp', repoZip, `sj://gitbackup/${repo.full_name}.zip` ])
+	}
+
+	/*
+	const userPath = `${__dirname}/repos/${username}`;
+
+	const totalSize = Number((await execa('du', [ '-sb', './' ], {
+		cwd: userPath
+	})).stdout.split('\t')[0]);
+
+	console.log({totalSize});
+	*/
+
+	return {
+		totalRepos: repos.length
+	};
+}
+
+(async () => {
+	for(; ;) {
+		const username = (await axios.post('http://localhost:8000/lock')).data;
+
+		try {
+			const updateLock = setInterval(async () => {
+				await axios.post(`http://localhost:8000/lock/${username}`)
+			}, 5000);
+
+			const {
+				totalRepos
+			} = await cloneUser(username);
+
+			clearInterval(updateLock);
+
+			await axios.post(`http://localhost:8000/lock/${username}/complete`, null, {
+				params: {
+					totalRepos
+				}
+			});
+		} catch(error) {
+			console.log(error);
+
+			await axios.post(`http://localhost:8000/lock/${username}/error`);
+		}
+	}
+})();
