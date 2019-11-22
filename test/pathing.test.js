fcc97ce79e0315c2bc40f57b234293a072551fcd
Monty Anderson <work@montyanderson.net>
2019-11-22 14:23:54 +0000

    test error handling

--- test/pathing.test.js
+++ test/pathing.test.js
@@ -2,8 +2,14 @@ const pathing = require('../lib/pathing');
 
 test('encode', async () => {
 	expect(pathing.encode('montyanderson')).toBe('/bc/be/22/6e/montyanderson');
+	expect(pathing.encode('calebcase')).toBe('/70/d1/ea/99/calebcase');
 });
 
 test('decode', async () => {
 	expect(pathing.decode('/bc/be/22/6e/montyanderson')).toBe('montyanderson');
+	expect(pathing.decode('/70/d1/ea/99/calebcase')).toBe('calebcase');
+
+	expect(() => {
+		pathing.decode('/be/be/22/6e/montyanderson')
+	}).toThrow(Error);
 });
