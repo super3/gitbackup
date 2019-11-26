a5acdaf4f5c4545d93c19d54af6b3aebcd0cc17b
Monty Anderson <work@montyanderson.net>
2019-11-26 17:12:10 +0000

    central bucket property

--- test/pathing.test.js
+++ test/pathing.test.js
@@ -1,15 +1,15 @@
 const pathing = require('../lib/pathing');
 
 test('encode', async () => {
-	expect(pathing.encode('montyanderson')).toBe('us-central-1:github.com/bc/be/22/6e/montyanderson');
-	expect(pathing.encode('calebcase')).toBe('europe-west-1:github.com/70/d1/ea/99/calebcase');
+	expect(pathing.encode('montyanderson')).toBe(`us-central-1:${pathing.bucket}/bc/be/22/6e/montyanderson`);
+	expect(pathing.encode('calebcase')).toBe(`europe-west-1:${pathing.bucket}/70/d1/ea/99/calebcase`);
 });
 
 test('decode', async () => {
-	expect(pathing.decode('us-central-1:github.com/bc/be/22/6e/montyanderson')).toBe('montyanderson');
-	expect(pathing.decode('europe-west-1:github.com/70/d1/ea/99/calebcase')).toBe('calebcase');
+	expect(pathing.decode(`us-central-1:${pathing.bucket}/bc/be/22/6e/montyanderson`)).toBe('montyanderson');
+	expect(pathing.decode(`europe-west-1:${pathing.bucket}/70/d1/ea/99/calebcase`'`)).toBe('calebcase');
 
 	expect(() => {
-		pathing.decode('us-central-1:github.com/be/be/22/6e/montyanderson');
+		pathing.decode(`us-central-1:${pathing.bucket}/be/be/22/6e/montyanderson`);
 	}).toThrow(Error);
 });
