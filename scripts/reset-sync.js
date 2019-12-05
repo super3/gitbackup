43609654cdad640a8f9f492589de1816e0162d67
Monty Anderson <work@montyanderson.net>
2019-12-05 01:17:56 +0000

    update reset sync script

--- scripts/reset-sync.js
+++ scripts/reset-sync.js
@@ -5,6 +5,7 @@ const redis = require('../redis');
 	await redis.multi()
 		.zunionstore('tracked', 1, 'tracked', 'WEIGHTS', 0)
 		.eval("local keys = redis.call('keys', ARGV[1]) \n for i=1,#keys,5000 do \n redis.call('del', unpack(keys, i, math.min(i+4999, #keys))) \n end \n return keys", 0, "user:*")
+		.eval("local keys = redis.call('keys', ARGV[1]) \n for i=1,#keys,5000 do \n redis.call('del', unpack(keys, i, math.min(i+4999, #keys))) \n end \n return keys", 0, "stats:*")
 		.del("active-workers")
 		.exec()
 
