226a3582536d6f7e5e94a0b7880402ea021719aa
Monty Anderson <work@montyanderson.net>
2019-12-05 01:12:24 +0000

    make resetting sync easier

--- scripts/reset-sync.js
+++ scripts/reset-sync.js
@@ -2,7 +2,11 @@ const fs = require('fs');
 const redis = require('../redis');
 
 (async () => {
-	await redis.zunionstore('tracked', 1, 'tracked', 'WEIGHTS', 0);
+	await redis.multi()
+		.zunionstore('tracked', 1, 'tracked', 'WEIGHTS', 0);
+		.eval("local keys = redis.call('keys', ARGV[1]) \n for i=1,#keys,5000 do \n redis.call('del', unpack(keys, i, math.min(i+4999, #keys))) \n end \n return keys", 0, "user:*")
+		.del("active-workers")
+		.exec()
 
 	process.exit(0);
 })();
