2cb76e4ebd99c87332de90c9c68dcc373c66a1c9
Monty Anderson <work@montyanderson.net>
2019-12-05 01:14:26 +0000

    fix typo

--- scripts/reset-sync.js
+++ scripts/reset-sync.js
@@ -3,7 +3,7 @@ const redis = require('../redis');
 
 (async () => {
 	await redis.multi()
-		.zunionstore('tracked', 1, 'tracked', 'WEIGHTS', 0);
+		.zunionstore('tracked', 1, 'tracked', 'WEIGHTS', 0)
 		.eval("local keys = redis.call('keys', ARGV[1]) \n for i=1,#keys,5000 do \n redis.call('del', unpack(keys, i, math.min(i+4999, #keys))) \n end \n return keys", 0, "user:*")
 		.del("active-workers")
 		.exec()
